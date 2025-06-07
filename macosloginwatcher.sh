a3a5daee15fadaea8c74d4cbef93ebe629b6445a
Alexander Karpov <karpulix@gmail.com>
2025-06-08 02:28:50 +0400

    version 1.0.8

--- macosloginwatcher.sh
+++ macosloginwatcher.sh
@@ -5,7 +5,7 @@ set -o pipefail
 trap 'echo "Error on line $LINENO"' ERR
 
 # Add at the beginning after other variables
-VERSION="1.0.7"
+VERSION="1.0.8"
 
 CONFIG_DIR="$HOME/.config/macosloginwatcher"
 CONFIG_FILE="$CONFIG_DIR/config"
